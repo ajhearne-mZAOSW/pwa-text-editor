c8974562e1cd99e9cd0dd9f972eb18bc6a8722cb
Alana Hearne <ajhearne@live.com>
2024-10-13 22:19:16 +1100

    sw error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = () => {
       // service worker
 			new InjectManifest({
 				swSrc: './src-sw.js',
-				swDest: 'src-sw.js',
+				swDest: './src-sw.js',
 			}),
 
       // manifest
